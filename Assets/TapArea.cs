0b09f00473999ec6865465d13330c62e39ffc753
AndreyD <56178406+DheANDREY@users.noreply.github.com>
2021-09-12 06:29:09 +0700

    GameIncremental_Agate

---
+++ Assets/TapArea.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+using UnityEngine.EventSystems;
+
+
+
+public class TapArea : MonoBehaviour, IPointerDownHandler
+
+{
+    public AudioSource fxfx1;
+    public void OnPointerDown(PointerEventData eventData)
+
+    {
+        
+        GameManager.Instance.CollectByTap(eventData.position, transform);
+        fxfx1.Play();
+    }
+
+}
